2e81111f1f9a741bbad4216a79ea46b25992e2fd
Fenslau <c01@bk.ru>
2025-02-22 13:19:13 +0700

    under constuction

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -8,32 +8,34 @@ use Illuminate\Support\Facades\Http;
 
 class UserService
 {
-    public function sendUserData(User $user, null|string $oldPassword = null)
+    public function sendUserData(User $user, null|string $oldPassword = null): void
     {
-        $data = [
-            'token' => '0xb1be70e9a83f19192cb593935ec4e2e2',
-            'req' => 'registeraccount2',
-            'login' => $user->name,
-            'email' => $user->email,
-            'passw' => $user->password,
-        ];
-        if (!is_null($oldPassword)) {
-            $data['oldpass'] = $oldPassword;
-        }
-        $response = Http::post('http://203.31.40.5:9246', $data);
-
-        $responseData = $response->json();
-
-        if (isset($responseData['resp'])) {
-            if ($responseData['resp'] === 'OK') {
-                logger()->info('Обновлён пользователь: ' . $user->email);
-            } elseif ($responseData['resp'] === 'ERR') {
-                logger()->warning('Ошибка: ' . $responseData['reason']);
-                throw new ExternalServiceException('Ошибка: ' . $responseData['reason']);
+        if (env('EXTERNAL_SERVICE_SYNC')) {
+            $data = [
+                'token' => '0xb1be70e9a83f19192cb593935ec4e2e2',
+                'req' => 'registeraccount2',
+                'login' => $user->name,
+                'email' => $user->email,
+                'passw' => $user->password,
+            ];
+            if (!is_null($oldPassword)) {
+                $data['oldpass'] = $oldPassword;
             }
-        } else {
-            logger()->error('Ошибка отправки пользователя: ' . $response->body());
-            throw new ExternalServiceException('Ошибка отправки пользователя: ' . $response->body());
+            $response = Http::post('http://203.31.40.5:9246', $data);
+
+            $responseData = $response->json();
+
+            if (isset($responseData['resp'])) {
+                if ($responseData['resp'] === 'OK') {
+                    logger()->info('Обновлён пользователь: ' . $user->email);
+                } elseif ($responseData['resp'] === 'ERR') {
+                    logger()->warning('Ошибка: ' . $responseData['reason']);
+                    throw new ExternalServiceException('Ошибка: ' . $responseData['reason']);
+                }
+            } else {
+                logger()->error('Ошибка отправки пользователя: ' . $response->body());
+                throw new ExternalServiceException('Ошибка отправки пользователя: ' . $response->body());
+            }
         }
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
 <div class="my-3 container-lg main">
     <div class="row">
-
+        <div class="my-5 h1 text-center text-success">В разработке</div>
         <div class="col-md-4 col-xl-3 mb-3 my-md-0 order-md-last">
             @include('inc.aside')
         </div>
