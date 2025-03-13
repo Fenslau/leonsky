2c155a1b0308f86d71f351d0610969cd61515175
Fenslau <c01@bk.ru>
2025-03-13 12:12:11 +0700

    minor fix

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,6 +2,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+    const containers = document.querySelectorAll('.card-body');
+
+    containers.forEach(container => {
+        const textBlock = container.querySelector('.card-text');
+
+        if (textBlock) {
+            const containerHeight = container.clientHeight;
+            const textBlockHeight = textBlock.scrollHeight;
+
+            if (textBlockHeight > containerHeight) {
+                textBlock.classList.add('text-overflow-container');
+            }
+        }
+    });
 });
 
 Echo.channel('server-status')
--- resources/views/article/list.blade.php
+++ resources/views/article/list.blade.php
@@ -2,15 +2,17 @@
   @foreach ($articles as $article)
   <div class="w-100">
     <div class="card h-100">
-      <div class="card-header">
-        <a class="nav-link position-relative pb-0" aria-current="true" href="{{ route('articles.show', $article->slug) }}">
+      <div class="card-header position-relative">
+        <a class="nav-link pb-0 d-flex justify-content-between" aria-current="true" href="{{ route('articles.show', $article->slug) }}">
           <h2 class="my-0">{{ $article->title }}</h2>
-          @if($article->isGlobal())
-          <span class="opacity-75 position-absolute bottom-0 end-0 badge rounded-pill text-bg-secondary"
-            data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Закреплено">
-            <i class="fa fa-thumb-tack" aria-hidden="true"></i>
-          </span>
-          @endif
+          <div class="d-flex justify-content-around flex-column align-items-end">
+            @if($article->isGlobal())
+            <span class="opacity-75 bottom-0 end-0 badge rounded-pill text-bg-secondary"
+              data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Закреплено">
+              <i class="fa fa-thumb-tack" aria-hidden="true"></i>
+            </span>
+            @endif
+          </div>
         </a>
       </div>
       <div class="card-body">
@@ -35,7 +37,7 @@
       </a>
       @endif
       <div style="max-height:400px;" class="card-body position-relative overflow-hidden">
-        <div class="card-text position-relative overflow-hidden text-overflow-container">
+        <div class="card-text position-relative overflow-hidden">
           @empty($article->highlights)
           {!! collect($article->content)->firstWhere('type', 'editor')['data']['editor'] ?? '' !!}
           @else
